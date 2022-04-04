8e7fd25e9f1056cb94ca1286c1096e568a74f860
wulong <wulong8641@yundasys.com>
2022-04-04 11:29:30 +0800

    脚本提交。，。

--- ddmc.js
+++ ddmc.js
@@ -1,4 +1,19 @@
 var nowTime = Date.parse(new Date())/1000;
+
+// 获取今日时间字符串
+function getTodayStr(time) {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    return year + "-" + month + "-" + day+" "+time;
+}
+
+// 时间字符串转时间戳
+function strToTimestamp(str) {
+    return Date.parse(new Date(str)) / 1000;
+}
+
 let data = {
     "success": true,
     "code": 0,
@@ -9,8 +24,8 @@ let data = {
             "default_select": false,
             "time": [
                 {
-                    "date_str": "今天(脚本破解)",
-                    "date_str_timestamp": nowTime,
+                    "date_str": "今天(来自Max破解)",
+                    "date_str_timestamp": this.nowTime,
                     "day": "today",
                     "times": [
                         {
@@ -21,8 +36,8 @@ let data = {
                             "textMsg": "",
                             "start_time": "08:30",
                             "end_time": "10:30",
-                            "start_timestamp": strToTimestamp(getTodayStr("08:30:00")),
-                            "end_timestamp": strToTimestamp(getTodayStr("10:30:00")),
+                            "start_timestamp": this.strToTimestamp(getTodayStr("08:30:00")),
+                            "end_timestamp": this.strToTimestamp(getTodayStr("10:30:00")),
                             "arrival_time_msg": "08:50-10:30",
                             "arrival_time": false,
                             "select_msg": "今天08:50-10:30"
@@ -35,8 +50,8 @@ let data = {
                             "textMsg": "",
                             "start_time": "10:30",
                             "end_time": "14:30",
-                            "start_timestamp": strToTimestamp(getTodayStr("10:30:00")),
-                            "end_timestamp": strToTimestamp(getTodayStr("14:30:00")),
+                            "start_timestamp": this.strToTimestamp(getTodayStr("10:30:00")),
+                            "end_timestamp": this.strToTimestamp(getTodayStr("14:30:00")),
                             "arrival_time_msg": "10:30-14:30",
                             "arrival_time": false,
                             "select_msg": "今天10:30-14:30"
@@ -49,8 +64,8 @@ let data = {
                             "textMsg": "",
                             "start_time": "14:30",
                             "end_time": "18:30",
-                            "start_timestamp": strToTimestamp(getTodayStr("14:30:00")),
-                            "end_timestamp": strToTimestamp(getTodayStr("18:30:00")),
+                            "start_timestamp": this.strToTimestamp(getTodayStr("14:30:00")),
+                            "end_timestamp": this.strToTimestamp(getTodayStr("18:30:00")),
                             "arrival_time_msg": "14:30-18:30",
                             "arrival_time": false,
                             "select_msg": "今天14:30-18:30"
@@ -63,8 +78,8 @@ let data = {
                             "textMsg": "",
                             "start_time": "18:30",
                             "end_time": "22:00",
-                            "start_timestamp": strToTimestamp(getTodayStr("18:30:00")),
-                            "end_timestamp": strToTimestamp(getTodayStr("22:00:00")),
+                            "start_timestamp": this.strToTimestamp(getTodayStr("18:30:00")),
+                            "end_timestamp": this.strToTimestamp(getTodayStr("22:00:00")),
                             "arrival_time_msg": "18:30-22:00",
                             "arrival_time": false,
                             "select_msg": "今天18:30-22:00"
@@ -85,20 +100,6 @@ let data = {
     "tradeTag": "success",
     "server_time": 1649033111,
     "is_trade": 1
-}
-
-// 获取今日时间字符串
-function getTodayStr(time) {
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    return year + "-" + month + "-" + day+" "+time;
-}
-
-// 时间字符串转时间戳
-function strToTimestamp(str) {
-    return Date.parse(new Date(str)) / 1000;
 }
 
 $done({ body: JSON.stringify(data) });
