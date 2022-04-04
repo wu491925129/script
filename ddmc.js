f0361881107dc615fb4756aaf773714d932a8e4e
wulong <wulong8641@yundasys.com>
2022-04-04 11:39:08 +0800

    脚本提交。，。

--- ddmc.js
+++ ddmc.js
@@ -1,17 +1,13 @@
 var nowTime = Date.parse(new Date())/1000;
 
-// 获取今日时间字符串
-function getTodayStr(time) {
+// 获取时间戳
+function strToTimestamp(time) {
     var date = new Date();
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
     var day = date.getDate();
-    return year + "-" + month + "-" + day+" "+time;
-}
-
-// 时间字符串转时间戳
-function strToTimestamp(str) {
-    return Date.parse(new Date(str)) / 1000;
+    var pushDate =  year + "-" + month + "-" + day+" "+time;
+    return Date.parse(new Date(pushDate)) / 1000;
 }
 
 var data = {
@@ -36,8 +32,8 @@ var data = {
                             "textMsg": "",
                             "start_time": "08:30",
                             "end_time": "10:30",
-                            "start_timestamp": this.strToTimestamp(getTodayStr("08:30:00")),
-                            "end_timestamp": this.strToTimestamp(getTodayStr("10:30:00")),
+                            "start_timestamp": strToTimestamp("08:30:00"),
+                            "end_timestamp": strToTimestamp("10:30:00"),
                             "arrival_time_msg": "08:50-10:30",
                             "arrival_time": false,
                             "select_msg": "今天08:50-10:30"
@@ -50,8 +46,8 @@ var data = {
                             "textMsg": "",
                             "start_time": "10:30",
                             "end_time": "14:30",
-                            "start_timestamp": this.strToTimestamp(getTodayStr("10:30:00")),
-                            "end_timestamp": this.strToTimestamp(getTodayStr("14:30:00")),
+                            "start_timestamp": strToTimestamp("10:30:00"),
+                            "end_timestamp": strToTimestamp("14:30:00"),
                             "arrival_time_msg": "10:30-14:30",
                             "arrival_time": false,
                             "select_msg": "今天10:30-14:30"
@@ -64,8 +60,8 @@ var data = {
                             "textMsg": "",
                             "start_time": "14:30",
                             "end_time": "18:30",
-                            "start_timestamp": this.strToTimestamp(getTodayStr("14:30:00")),
-                            "end_timestamp": this.strToTimestamp(getTodayStr("18:30:00")),
+                            "start_timestamp": strToTimestamp("14:30:00"),
+                            "end_timestamp": strToTimestamp("18:30:00"),
                             "arrival_time_msg": "14:30-18:30",
                             "arrival_time": false,
                             "select_msg": "今天14:30-18:30"
@@ -78,8 +74,8 @@ var data = {
                             "textMsg": "",
                             "start_time": "18:30",
                             "end_time": "22:00",
-                            "start_timestamp": this.strToTimestamp(getTodayStr("18:30:00")),
-                            "end_timestamp": this.strToTimestamp(getTodayStr("22:00:00")),
+                            "start_timestamp": strToTimestamp("18:30:00"),
+                            "end_timestamp": strToTimestamp("22:00:00"),
                             "arrival_time_msg": "18:30-22:00",
                             "arrival_time": false,
                             "select_msg": "今天18:30-22:00"
