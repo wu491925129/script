bfd62656be8bfd5586cfce2fc8b1bce7ae2003f4
wulong <wulong8641@yundasys.com>
2022-04-08 06:42:56 +0800

    脚本提交。，。

--- checkOrderDirect.js
+++ checkOrderDirect.js
@@ -13,8 +13,11 @@ var data = {
 
 var res = JSON.parse($response.body);
 
+console.log("重写订单校验")
+console.log($response.body)
+
 if (!res.success) {
-    $done({body: JSON.stringify(data)});
+    $done({ body: JSON.stringify(data) });
 } else {
     $done({ body: $response.body });
 }
